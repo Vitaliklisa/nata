faaf8250e98bb7374cdb2c5893d63255067286e3
vitaliikhomenko <vhomenko119@gmail.com>
2023-03-01 19:01:49 -0500

    Add CI

--- lib/presentation/features/screen/main.dart
+++ lib/presentation/features/screen/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nata/firebase_options.dart';
 import 'package:nata/presentation/features/screen/home_page.dart';
 
-void main() => runApp(NataApp());
+void main() {
+   Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(NataApp());
+}
 
 class NataApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
