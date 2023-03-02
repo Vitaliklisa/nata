9a6699bfc160365943bfbcd6f37a8f15d9093a1f
vitaliikhomenko <vhomenko119@gmail.com>
2023-03-01 19:12:01 -0500

    move main out of presentation layer

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nata/presentation/features/screen/main.dart';
+import 'package:nata/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
